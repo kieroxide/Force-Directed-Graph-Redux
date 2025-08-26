6e86e6b1dba921d72116cf52c183001a11e95524
Kieroxide <kieranbprivate:gmail.com>
2025-08-26 20:20:41 +0100

    refactored to use the initalised colours

--- FDG/src/classes/Edge.ts
+++ FDG/src/classes/Edge.ts
@@ -4,7 +4,7 @@ export class Edge {
     source: Vertex;
     target: Vertex;
     type: string;
-
+    lineColour?: string;
     constructor(vertexA: Vertex, vertexB: Vertex, type: string) {
         this.source = vertexA;
         this.target = vertexB;
@@ -17,8 +17,8 @@ export class Edge {
     draw(ctx: CanvasRenderingContext2D) {
         const { source, target } = this;
 
-        ctx.strokeStyle = "#18143fff";
-        ctx.lineWidth = 2;
+        ctx.strokeStyle = this.lineColour || "#00000012";
+        ctx.lineWidth = 4;
         ctx.beginPath();
         ctx.moveTo(source.pos.x, source.pos.y);
         ctx.lineTo(target.pos.x, target.pos.y);

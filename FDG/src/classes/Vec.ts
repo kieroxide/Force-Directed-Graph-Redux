f10958c45a686c179b3f2294aa9c25a75a93221d
Kieroxide <kieranbprivate:gmail.com>
2025-08-27 16:01:38 +0100

    Added canvas to world function to Vec class

--- FDG/src/classes/Vec.ts
+++ FDG/src/classes/Vec.ts
@@ -1,3 +1,5 @@
+import type { Camera } from "./Camera";
+
 export class Vec {
     x: number;
     y: number;
@@ -5,5 +7,12 @@ export class Vec {
     constructor(x: number, y: number) {
         this.x = x;
         this.y = y;
+    }
+
+    canvasToWorld(camera: Camera) {
+        // returns world space Vector/Coordinate from canvas space coord
+        const worldX = (this.x - camera.pos.x) / camera.zoom;
+        const worldY = (this.y - camera.pos.y) / camera.zoom;
+        return new Vec(worldX, worldY);
     }
 }

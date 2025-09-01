b44b9b10bb126c57ff2ae0c496a2382782875875
Kieroxide <kieranbprivate:gmail.com>
2025-09-01 22:42:29 +0100

    Updated Edge to use new format

--- FDG/src/classes/Edge.ts
+++ FDG/src/classes/Edge.ts
@@ -1,27 +1,40 @@
+import type { Graph } from "./Graph";
 import { Vertex } from "./Vertex";
 
 export class Edge {
-    source: Vertex;
-    target: Vertex;
+    sourceId: string;
+    sourceRef: Vertex;
+    targetId: string;
+    targetRef: Vertex;
     type: string;
     lineColour?: string;
-    constructor(vertexA: Vertex, vertexB: Vertex, type: string) {
-        this.source = vertexA;
-        this.target = vertexB;
-        this.type = type;
 
-        vertexA.edges.push(this);
-        vertexB.edges.push(this);
+    constructor(
+        sourceID: string,
+        targetID: string,
+        type: string,
+        graph: Graph
+    ) {
+        this.sourceId = sourceID;
+        this.targetId = targetID;
+        this.type = type;
+        this.sourceRef = graph.getVertex(sourceID);
+        this.targetRef = graph.getVertex(targetID);
+        
+        if (!this.sourceRef || !this.targetRef) {
+            throw new Error(`Invalid vertex IDs: source=${sourceID}, target=${targetID}`);
+        }
+        
+        this.sourceRef.edges.push(this);
+        this.targetRef.edges.push(this);
     }
 
     draw(ctx: CanvasRenderingContext2D) {
-        const { source, target } = this;
-
         ctx.strokeStyle = this.lineColour || "#00000012";
         ctx.lineWidth = 4;
         ctx.beginPath();
-        ctx.moveTo(source.pos.x, source.pos.y);
-        ctx.lineTo(target.pos.x, target.pos.y);
+        ctx.moveTo(this.sourceRef.pos.x, this.sourceRef.pos.y);
+        ctx.lineTo(this.targetRef.pos.x, this.targetRef.pos.y);
         ctx.stroke();
     }
 }

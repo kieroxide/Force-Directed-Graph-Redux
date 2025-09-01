37e2cc4747634232d1e34d169ea626c6f8b7c6ae
Kieroxide <kieranbprivate:gmail.com>
2025-09-01 22:26:25 +0100

    Updated file reading interfaces as updated graph data structure

--- FDG/src/objects/importedGraph.ts
+++ FDG/src/objects/importedGraph.ts
@@ -1,18 +1,19 @@
 // These interfaces are type declarations for the imported graph data
 // TODO: Make wikidata reader not just for games
 
-export interface ObjNode {
-    type: string;
-    releaseDate?: string;
+// Maps an entity QID to its label
+export interface EntityMap {
+  [entity_id: string]: string;
 }
 
-export interface ObjEdge {
-    source: string;
-    target: string;
-    type: string;
+// Maps a property PID to its label
+export interface PropertyMap {
+  [property_id: string]: string;
 }
 
-export interface ObjGraphData {
-    nodes: Record<string, ObjNode>;
-    edges: ObjEdge[];
+// Maps a source entity to its outgoing relationships
+export interface RelationshipsMap {
+  [source_id: string]: {
+    [property_id: string]: string[]; // Array of target QIDs
+  };
 }

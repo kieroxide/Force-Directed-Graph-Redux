32088420c216ebd9903049ac4a459d3ff7c1d0bf
Kieroxide <kieranbprivate:gmail.com>
2025-09-04 20:15:15 +0100

    Refactored Camera class to use private fields and also contains moved functions more appropriate for camera

--- FDG/frontend/src/classes/Camera.ts
+++ FDG/frontend/src/classes/Camera.ts
@@ -1,29 +1,37 @@
-import { Controls } from "../constants";
-import { Vec } from "./Vec";
+import { Controls } from "../../constants";
+import { Vec } from "../graph/Vec";
 
 export class Camera {
-    pos: Vec;
-    zoom: number;
+    private _pos: Vec;
+    private _zoom: number;
+
     constructor() {
-        this.pos = new Vec(0, 0);
-        this.zoom = 1;
+        this._pos = new Vec(0, 0);
+        this._zoom = 1;
+    }
+
+    canvasToWorld(vector: Vec) {
+        // returns world space Vector/Coordinate from canvas space coord
+        const worldX = (vector.x - this._pos.x) / this._zoom;
+        const worldY = (vector.y - this._pos.y) / this._zoom;
+        return new Vec(worldX, worldY);
     }
 
     applyTransform(ctx: CanvasRenderingContext2D) {
-        ctx.translate(this.pos.x, this.pos.y);
-        ctx.scale(this.zoom, this.zoom);
+        ctx.translate(this._pos.x, this._pos.y);
+        ctx.scale(this._zoom, this._zoom);
     }
 
     pan(dx: number, dy: number) {
-        this.pos.x += dx * Controls.MouseSpeedFactor;
-        this.pos.y += dy * Controls.MouseSpeedFactor;
+        this._pos.x += dx * Controls.MouseSpeedFactor;
+        this._pos.y += dy * Controls.MouseSpeedFactor;
     }
 
     zoomAt(c_mouse: Vec, ws_mouse: Vec, factor: number) {
-        this.zoom *= factor;
+        this._zoom *= factor;
 
         // Adjust offset to keep mouse point fixed
-        this.pos.x = c_mouse.x - ws_mouse.x * this.zoom;
-        this.pos.y = c_mouse.y - ws_mouse.y * this.zoom;
+        this._pos.x = c_mouse.x - ws_mouse.x * this._zoom;
+        this._pos.y = c_mouse.y - ws_mouse.y * this._zoom;
     }
 }

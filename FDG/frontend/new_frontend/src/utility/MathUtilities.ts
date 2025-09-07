e892699185a0bf159faa61aab57890d1e49ff59a
Kieroxide <kieranbprivate:gmail.com>
2025-09-07 17:30:13 +0100

    Config

--- FDG/frontend/new_frontend/src/utility/MathUtilities.ts
+++
@@ -1,8 +0,0 @@
-export class MathUtility {
-    /**
-     * Clamps a value between -absMaxVal and +absMaxVal
-     */
-    static clamp(val: number, absMaxVal: number) {
-        return Math.max(-absMaxVal, Math.min(val, absMaxVal));
-    }
-}

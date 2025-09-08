8710b7ce2b5e6367cdf9db7b43860a4ea3b5265a
Kieroxide <kieranbprivate:gmail.com>
2025-09-08 16:52:54 +0100

    Refactored Vertex draw function with a new cache field to avoid calling costly calculations. Added image functionality to Vertex and used roundRect instead of regular Rects

--- FDG/frontend/src/graph/Vertex.ts
+++ FDG/frontend/src/graph/Vertex.ts
@@ -1,13 +1,13 @@
 import { Vec } from "./Vec";
 import { Edge } from "./Edge";
-import { VERTEX_FONT } from "../../constants/font";
 import { VertexUtility } from "../utility/VertexUtility";
 import { MathUtility } from "../utility/MathUtility";
-import { CanvasUtility } from "../utility/CanvasUtility";
 
 export class Vertex {
     private static readonly MAX_SPEED = 20;
     private static readonly DAMPING = 0.9;
+    private static RECT_RADIX = 40;
+
     // Euclidean Data
     private readonly _pos: Vec;
     get pos() {
@@ -31,9 +31,10 @@ export class Vertex {
     set selected(isSelected: boolean) {
         this._selected = isSelected;
     }
+
     // Generic Data
     private readonly _name: string;
-
+    
     private readonly _id: string;
     get id() {
         return this._id;
@@ -44,26 +45,43 @@ export class Vertex {
         return this._type;
     }
 
+    img: HTMLImageElement | undefined;
     // Visual Label
     private readonly _label: string;
     get label() {
         return this._label;
     }
 
-    private readonly _TEXT_BOX: {
-        readonly PADDING_WIDTH: number;
-        readonly PADDING_HEIGHT: number;
-        readonly BORDER_COLOR: string;
-        readonly BORDER_WIDTH: number;
-    };
-    get TEXT_BOX() {
-        return this._TEXT_BOX;
-    }
-
     public textWidth?: number;
+    public textHeight?: number;
     labelColour?: string;
 
-    constructor(id: string, name: string, type: string = "") {
+    _cachedDimensions?: {
+        // Text measurements
+        labelWidth: number;
+        labelHeight: number;
+        typeWidth: number;
+        typeHeight: number;
+        maxTextWidth: number;
+        maxTextHeight: number;
+
+        // Layout measurements
+        imgSize: number;
+        boxWidth: number;
+        boxHeight: number;
+
+        // Spacing constants
+        padding: number;
+        gap: number;
+
+        // Cache keys
+        fontSize: number;
+        label: string;
+        type: string;
+        hasImage: boolean;
+    };
+
+    constructor(id: string, name: string, type: string = "", imgURL: string = "", ctx: CanvasRenderingContext2D) {
         this._pos = new Vec(200, 200);
         this._velocity = new Vec(0, 0);
         this._id = id;
@@ -73,14 +91,13 @@ export class Vertex {
         this._connectedEdges = [];
 
         this._label = this._name;
+        if (imgURL != "") {
+            this.img = new Image();
+            this.img.src = imgURL;
+        }
         this.labelColour = undefined;
 
-        this._TEXT_BOX = {
-            PADDING_WIDTH: 50,
-            PADDING_HEIGHT: 70,
-            BORDER_COLOR: "#000000ff",
-            BORDER_WIDTH: 3,
-        };
+        this._cachedDimensions = VertexUtility.ensureValidCache(ctx, this);
     }
 
     /**
@@ -96,35 +113,30 @@ export class Vertex {
     }
 
     draw(ctx: CanvasRenderingContext2D) {
-        // Calibrates font
-        const massFontSize =
-            VERTEX_FONT.SIZE + VertexUtility.getOriginalMass(this) * VERTEX_FONT.MASS_WEIGHT;
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        ctx.font = CanvasUtility.getFontString(VERTEX_FONT.FAMILY, massFontSize);
+        const cache = VertexUtility.ensureValidCache(ctx, this)!;
 
-        // Calculates boxWidth and Height. Could be cached
-        const width = VertexUtility.getTextWidth(ctx, this);
-        const boxWidth = width + this._TEXT_BOX.PADDING_WIDTH;
-        const boxHeight = massFontSize + this._TEXT_BOX.PADDING_HEIGHT;
+        // Calculate positions dynamically (never cached)
+        const boxLeft = this.pos.x - cache.boxWidth / 2;
+        const boxTop = this.pos.y - cache.boxHeight / 2;
 
         // Draw background box
         ctx.fillStyle = this.labelColour || "grey";
-        // Drawn where the box midpoint is the position vector
-        ctx.fillRect(this._pos.x - boxWidth / 2, this._pos.y - boxHeight / 2, boxWidth, boxHeight);
+        ctx.beginPath();
+        ctx.roundRect(boxLeft, boxTop, cache.boxWidth, cache.boxHeight, Vertex.RECT_RADIX);
+        ctx.fill();
 
-        // Draw border
-        if (this._selected) {
-            ctx.strokeStyle = "#22fff0ff"; // White border when selected
-            ctx.lineWidth = 4;
+        const hasImage = cache.hasImage;
+        if (hasImage) {
+            VertexUtility.drawImageVertex(ctx, this);
         } else {
-            ctx.strokeStyle = this._TEXT_BOX.BORDER_COLOR;
-            ctx.lineWidth = this._TEXT_BOX.BORDER_WIDTH;
+            VertexUtility.drawTextVertex(ctx, this);
         }
-        ctx.strokeRect(this._pos.x - boxWidth / 2, this._pos.y - boxHeight / 2, boxWidth, boxHeight);
 
-        // Draw text
-        ctx.fillStyle = "#000000ff";
-        ctx.fillText(this._label, this._pos.x, this._pos.y);
+        // Draw box border
+        ctx.strokeStyle = this._selected ? "yellow" : "black";
+        ctx.lineWidth = this._selected ? 3 : 1;
+        ctx.beginPath();
+        ctx.roundRect(boxLeft, boxTop, cache.boxWidth, cache.boxHeight, Vertex.RECT_RADIX);
+        ctx.stroke();
     }
 }

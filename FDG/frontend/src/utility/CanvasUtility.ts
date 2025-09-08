de1619f1d144f2afa0ba0676322fb8efd72beaf3
Kieroxide <kieranbprivate:gmail.com>
2025-09-08 16:57:18 +0100

    Added a starting palette for nicer starting colours

--- FDG/frontend/src/utility/CanvasUtility.ts
+++ FDG/frontend/src/utility/CanvasUtility.ts
@@ -8,18 +8,50 @@ export class CanvasUtility {
         SATURATION_MAX: 50,
         LIGHTNESS_MIN: 40,
         LIGHTNESS_MAX: 60,
+    };
+
+    private static readonly PALETTE = [
+        "#4F8A8B",
+        "#FBD46D",
+        "#F76B8A",
+        "#A3D8F4",
+        "#374785",
+        "#24305E",
+        "#70A1D7",
+        "#F8E9A1",
+        "#A1DE93",
+        "#FFB6B9",
+        "#6A0572",
+        "#AB83A1",
+        "#F67280",
+        "#355C7D",
+        "#C06C84",
+    ];
+
+    private static _paletteIndex = 0;
+
+    static nextNiceColor() {
+        const color = CanvasUtility.PALETTE[CanvasUtility._paletteIndex % CanvasUtility.PALETTE.length];
+        CanvasUtility._paletteIndex++;
+        return color;
     }
-    
+
     /**
      * Generates a random HSL color with pleasant saturation/lightness
      */
     static randomNiceColor() {
-        const hue = Math.floor(Math.random() * CanvasUtility.COLOURS.HUE_MAX) + CanvasUtility.COLOURS.HUE_MIN;
-        const saturation =
-            Math.floor(Math.random() * CanvasUtility.COLOURS.SATURATION_MAX) + CanvasUtility.COLOURS.SATURATION_MIN;
-        const lightness = Math.floor(Math.random() * CanvasUtility.COLOURS.LIGHTNESS_MAX) + CanvasUtility.COLOURS.LIGHTNESS_MIN;
-        return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+        if (this.PALETTE.length >= this._paletteIndex){
+            const hue = Math.floor(Math.random() * CanvasUtility.COLOURS.HUE_MAX) + CanvasUtility.COLOURS.HUE_MIN;
+            const saturation =
+                Math.floor(Math.random() * CanvasUtility.COLOURS.SATURATION_MAX) + CanvasUtility.COLOURS.SATURATION_MIN;
+            const lightness =
+                Math.floor(Math.random() * CanvasUtility.COLOURS.LIGHTNESS_MAX) + CanvasUtility.COLOURS.LIGHTNESS_MIN;
+            return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+        } else {
+            return this.nextNiceColor()
+        }
     }
+    
     /**
      * Converts browser mouse coordinates to canvas coordinates
      */
@@ -42,9 +74,7 @@ export class CanvasUtility {
     /**
      * Changes font size in a CSS font string
      */
-    static getFontString(fontStr: string, newSizePx: number): string {
-        // Regex matches the first number + 'px'
-        return fontStr.replace(/(\d+)px/, `${newSizePx}px`);
+    static getFontString(fontFamily: string, newSizePx: number): string {
+        return `bold ${newSizePx}px ${fontFamily}`;
     }
-
 }

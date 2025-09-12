9829fb3d240cba9b7c8d7ec3924b27010460ef44
Kieroxide <kieranbprivate@gmail.com>
2025-09-12 21:51:19 +0100

    Changed the mass weight to not be so extreme

--- FDG/frontend/constants/font.ts
+++ FDG/frontend/constants/font.ts
@@ -1,7 +1,7 @@
 export const VERTEX_FONT = {
     SIZE: 20,
     FAMILY: "sans-serif",
-    MASS_WEIGHT: 5,
+    MASS_WEIGHT: 3,
     get FULL() {
         return `bold ${this.SIZE}px ${this.FAMILY}`;
     },

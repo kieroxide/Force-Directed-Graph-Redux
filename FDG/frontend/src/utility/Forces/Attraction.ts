4666cef31a31a172c4ef2637b1605dd16ddcc94c
Kieroxide <kieranbprivate:gmail.com>
2025-09-16 19:26:20 +0100

    Tweaked centralSpring constant

--- FDG/frontend/src/utility/Forces/Attraction.ts
+++ FDG/frontend/src/utility/Forces/Attraction.ts
@@ -5,7 +5,7 @@ import { VertexUtility } from "../VertexUtility";
 
 export class Attraction {
     private static readonly SPRING = 0.025;
-    private static readonly CENTRAL_SPRING = 0.075;
+    private static readonly CENTRAL_SPRING = 0.05;
     private static readonly REST_LENGTH = 100;
 
     static centerAttraction(origins: Set<Vertex>, canvas: HTMLCanvasElement) {

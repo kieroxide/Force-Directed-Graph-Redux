fd13eeda6ad59905c1352454920fac7ac4a7adb1
Kieroxide <kieranbprivate:gmail.com>
2025-09-24 17:28:27 +0100

    Updated vite config to have correct paths when built

--- FDG/frontend/vite.config.ts
+++ FDG/frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    base: '/Force-Directed-Graph-Redux/',
     server: {
         proxy: {
             "/api": "http://localhost:5000",
